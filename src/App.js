419fd9357919121b38420a47b69d6d930d3f3c9f
dan make <danmakes2022@gmail.com>
2018-03-28 18:51:29 +0300

    Настройка React, Routers, Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import Root from "./components/Root";
+import store from "./redux";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
+import history from "./history";
+
+console.log(ConnectedRouter);
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div>
+            <Root />
+          </div>
+        </ConnectedRouter>
+      </Provider>
+    );
   }
 }
 
---
+++ src/redux/index.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import reducer from "./reducer";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import history from "../history";
+import { routerMiddleware } from "react-router-redux";
+
+const enhancer = applyMiddleware(routerMiddleware(history), thunk, logger);
+
+const store = createStore(reducer, enhancer);
+
+window.store = store;
+
+export default store;
